a3581fc516c9d5033c6edaa4961608db9c85d036
LucasRivero01 <lucas.rivero@naranjax.com>
2022-01-14 19:32:35 -0300

    se agrega salida en txt

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+const fs = require('fs');
+
 const factor = 5;
+let resultado = '';
 
 for (let i = 1; i <= 10; i++) {
-    console.log(`${factor} x ${i} = ${factor * i}`);
+    //\n para salto de linea
+    resultado += `${factor} x ${i} = ${factor * i}\n`;
 }
+fs.writeFile('tabla-del-5.txt', resultado, (err) => {
+    if (err) throw err;
+    console.log('tabla-del-5.txt creada');
+})
